76952067d560bdfbc41f67f32d28b214a1be7e2b
Stephen <stephenrugh@gmail.com>
2014-06-14 13:10:55 -0500

    Fixed tileset background Added holes, wall and portal collisions

--- robits.js
+++ robits.js
@@ -5,7 +5,7 @@ var w = window,
     width = w.innerWidth || e.clientWidth || g.clientWidth,
     height = w.innerHeight || e.clientHeight || g.clientHeight;
 
-var game = new Phaser.Game(width, height, Phaser.CANVAS, 'robits', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'robits', { preload: preload, create: create, update: update, render: render });
 var localPlayer, board, map;
 var cursors;
 var widthInTiles, heightInTiles, tileWidth;
@@ -23,17 +23,24 @@ function preload() {
     game.load.tilemap('map', 'assets/map1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tileset', 'assets/tileset.png');
     game.load.image('robot', 'assets/robot.png');
+    game.load.image('portal', 'assets/portal.png');
 }
 
 function create() {
 
-    setUpSocketReceivers()
+    setUpSocketReceivers();
     cursors = game.input.keyboard.createCursorKeys();
+
+    game.stage.backgroundColor  = '#787878';
 
     map = game.add.tilemap('map');
     map.addTilesetImage('tileset');
 
-    var layer = map.createLayer('Tile Layer 1');
+    map.setCollision(6);
+    map.setTileIndexCallback(3, goThroughPortal, this);
+    map.setTileIndexCallback(5, fallInHole, this);
+
+    layer = map.createLayer('Tile Layer 1');
     layer.resizeWorld();
 
     game.world.setBounds(0, 0, map.width, map.height);
@@ -63,7 +70,9 @@ function addPlayer(data) {
     });
 
     player.body.collideWorldBounds = true;
-    player.anchor.setTo(0.5, 0.5);
+  player.body.setSize(128, 128); //TODO set to 128x128 once we have perfect movement
+
+  player.anchor.setTo(0.5, 0.5);
 
     var color = colorScale(players.length / maxPlayers);
     player.tint = parseInt(color.hex().replace("#", ""), 16);
@@ -75,13 +84,14 @@ function addPlayer(data) {
 
 function update() {
     _.each(players, function (player) {
-        planMovement(player);
+      game.physics.arcade.collide(player, layer);
+      planMovement(player);
         kickoffMovement(player);
     });
 }
 
 function render() {
-
+  game.debug.body(localPlayer);
 }
 
 function planMovement(player) {
@@ -126,32 +136,51 @@ function moveAtAngle(player, angle) {
 
     game.physics.arcade.velocityFromAngle(angle || 0, speed, player.body.velocity);
 
-    setTimeout(function () {
-        player.body.velocity.x = 0;
-        player.body.velocity.y = 0;
-        player.data.stepInProgress = false;
-    }, time * 1000);
+    setTimeout(_.partial(clearSpriteMovement, player), time * 1000);
 }
 
 function disconnect() {
     socket.emit("player left", playerId);
 }
 
-function broadcastMove() {
-    socket.emit("player moved", 0);
+function playerDisconnected(){
+
+    socket.emit("player left", playerId )
 }
 
-function localPlayerDies() {
-    socket.emit("player died", playerId);
+function playerMoved() {
+    socket.emit("player moved", 0)
 }
 
-function localPlayerReachesCheckpoint() {
-    socket.emit("checkpoint reached", playerId);
+function playerDied() {
+    socket.emit("player died", playerId)
+}
+
+function goThroughPortal(sprite, tile) {
+  console.log("going through portal!");
+  //teleport to other portal
+  return false;
+}
+
+function fallInHole(sprite, tile) {
+  console.log("fell in hole!");
+  // player reset
+  sprite.body.x = 0;
+  sprite.body.y = 0;
+  clearSpriteMovement(sprite);
+  return false;
+}
+
+function clearSpriteMovement(sprite) {
+  sprite.body.velocity.x = 0;
+  sprite.body.velocity.y = 0;
+  sprite.data.stepInProgress = false;
 }
 
 function playerConnected(playerId) {
     addPlayer({id: playerId});
 }
+
 
 function playerWins(playerId) {
     //stop the game. display ./vbcn/message
